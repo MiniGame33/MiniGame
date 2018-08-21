5d974540369c161e1068be87c3c504702385e1e5
millionwang <291464890@qq.com>
2018-08-21 20:12:57 +0800

    //事件跳动动画

---
+++ minigame33/Assets/Scripts/MainScene/EventItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class EventItem : MonoBehaviour {
+    public Vector3 startPos;
+    public Vector3 endPos;
+	// Use this for initialization
+	void Start () {
+        startPos = transform.localPosition;
+        endPos = startPos + new Vector3(0, 0.5f, 0);
+        transform.DOLocalMove(endPos, 0.5f).SetLoops(-1,LoopType.Yoyo);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
