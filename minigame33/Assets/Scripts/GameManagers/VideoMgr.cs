b5ffe09531640ce8f2ee90c6fc5ac1c2b464a5e6
millionwang <millionwang@tencent.com>
2018-09-14 17:24:53 +0800

    //视频

---
+++ minigame33/Assets/Scripts/GameManagers/VideoMgr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoMgr : MonoBehaviour {
+    public static VideoMgr _instance;
+    public VideoPlayer videoPlayer;
+    public GameObject canvasGo;
+    private void Awake()
+    {
+        _instance = this;
+    }
+    // Use this for initialization
+    void Start () {
+        canvasGo.SetActive(false);
+        videoPlayer.Play();
+        Invoke("Finish",(float)videoPlayer.clip.length);
+    }
+    public void Finish() {
+        canvasGo.SetActive(true);
+        videoPlayer.Stop();
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
